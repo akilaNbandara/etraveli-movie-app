4f34aa12f0e4e4a86e05e9a2b04fe68023135e5e
akilanbandara <akilanbandara@gmail.com>
2026-01-20 09:28:00 +0100

    bugfix: remove unused dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { MoviePage } from './components/MoviePage';
 import { MovieProvider } from './state';
 
 function App() {
-  const queryClient = new QueryClient();
-
   return (
     <BrowserRouter basename="/etraveli-movie-app">
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <MovieProvider>
-                <MoviePage />
-              </MovieProvider>
-            }
-          />
-        </Routes>
-      </QueryClientProvider>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<MovieProvider>
+							<MoviePage />
+						</MovieProvider>
+					}
+				/>
+			</Routes>
     </BrowserRouter>
   );
 }
